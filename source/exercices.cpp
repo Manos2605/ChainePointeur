92a1a3d3c0e1469875856185d8d3296c632b9285
Manos2605 <sonwadimitry26@gmail.com>
2024-11-16 09:27:27 +0100

    Modification5

--- source/exercices.cpp
+++ source/exercices.cpp
@@ -66,27 +66,27 @@ void inverse(char* chaine) {
     int length = longueur(chaine);
     char invers;
     for (int i = 0; i < length / 2; i++) {
-        invers = chaine[i];
-        chaine[i] = chaine[length - 1 - i];
-        chaine[length - 1 - i] = invers;
+        invers = *(chaine+i);
+        *(chaine+i) = *(chaine+(length - 1 - i));
+        *(chaine+(length - 1 - i)) = invers;
     }
 }
 
 // La différence entre un caractère minuscule et son équivalent majuscule est de 32
 void to_upper(char* chaine){
     int i = 0;
-    while (chaine[i]!= '\0') {
-        if (chaine[i] >= 'a' && chaine[i] <= 'z') {
-            chaine[i] -= 32;
+    while (*(chaine+i)!= '\0') {
+        if (*(chaine+i) >= 'a' && *(chaine+i) <= 'z') {
+            *(chaine+i) -= 32;
         }
         i++;
     }
 }
 void to_lower(char* chaine){
     int i = 0;
-    while (chaine[i]!= '\0') {
-        if (chaine[i] >= 'A' && chaine[i] <= 'Z') {
-            chaine[i] += 32;
+    while (*(chaine+i)!= '\0') {
+        if (*(chaine+i) >= 'A' && *(chaine+i) <= 'Z') {
+            *(chaine+i) += 32;
         }
         i++;
     }
@@ -95,14 +95,14 @@ void to_lower(char* chaine){
 char * cherche_mot(const char* phrase, const char* mot){
     int length_mot = longueur(mot);
     int i = 0;
-    while (phrase[i]!= '\0') {
-        if (phrase[i] == mot[0]) {
+    while (*(phrase+i)!= '\0') {
+        if (*(phrase+i) == mot[0]) {
             int j = 1;
-            while (mot[j]!= '\0' && phrase[i+j]!= '\0' && phrase[i+j] == mot[j]) {
+            while (*(mot+j)!= '\0' && *(phrase+(i+j))!= '\0' && *(phrase+(i+j)) == *(mot+j)) {
                 j++;
             }
-            if (mot[j] == '\0') {
-                return (char*)&phrase[i];
+            if (*(mot+j) == '\0') {
+                return (char*)&*(phrase+i);
             }
         }
         i++;
@@ -114,19 +114,19 @@ char * cherche_mot(const char* phrase, const char* mot){
 void sous_chaine(const char* source, char* dest, int debut, int longueur){
     int i = 0;
     int j = debut;
-    while (j < debut + longueur + 1 && source[j]!= '\0') {
-        dest[i] = source[j - 2];
+    while (j < debut + longueur + 1 && *(source+j)!= '\0') {
+        *(dest+i) = *(source+(j - 2));
         i++;
         j++;
     }
-    dest[i] = '\0';
+    *(dest+i) = '\0';
 }
 
 void supprime_caractere(char* chaine, char caractere){
     int i = 0, j = 0;
-    while (chaine[i] != '\0') {
-        if (chaine[i] != caractere) {
-            chaine[j] = chaine[i];
+    while (*(chaine+i) != '\0') {
+        if (*(chaine+i) != caractere) {
+            *(chaine+j) = *(chaine+i);
             j++;
         }
         i++;
@@ -138,8 +138,8 @@ void supprime_caractere(char* chaine, char caractere){
 int compte_mots(const char* phrase){
     int count = 0;
     int i = 0;
-    while (phrase[i]!= '\0') {
-        if (phrase[i] == ' ') {
+    while (*(phrase+i)!= '\0') {
+        if (*(phrase+i) == ' ') {
             count++;
         }
         i++;
